264dbd2196d1af1dae6ee5ea2f7216f13f0e955c
Dara <dara.dandashi@gmail.com>
2020-07-20 21:22:12 +0300

    Updates

---
+++ precache-manifest.80c7f764136e6fbcaddd8d607937d3e0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c21421ce607c95ecdef573ed4a4beb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5fda68b6f62dd06f007b",
+    "url": "/portfolio/static/css/main.1221e3a1.chunk.css"
+  },
+  {
+    "revision": "896a460953e2b56225f1",
+    "url": "/portfolio/static/js/2.14c5a25f.chunk.js"
+  },
+  {
+    "revision": "dd322a405af58dd6f68be48e808e0f51",
+    "url": "/portfolio/static/js/2.14c5a25f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fda68b6f62dd06f007b",
+    "url": "/portfolio/static/js/main.82d14473.chunk.js"
+  },
+  {
+    "revision": "e5793b53fd44ff7c926f",
+    "url": "/portfolio/static/js/runtime-main.6a088b17.js"
+  },
+  {
+    "revision": "d751d3c8b9b7772486d3914dc58f3bd7",
+    "url": "/portfolio/static/media/CV.d751d3c8.pdf"
+  },
+  {
+    "revision": "6db0e97131dfe6c3c611dde5b0b7e93c",
+    "url": "/portfolio/static/media/LearnGoImg.6db0e971.png"
+  },
+  {
+    "revision": "b3dae2832215047d59e047300764d7ac",
+    "url": "/portfolio/static/media/MovieImg.b3dae283.PNG"
+  },
+  {
+    "revision": "2c4347591da55cff32908d83f8a45a48",
+    "url": "/portfolio/static/media/PorfolioImg.2c434759.PNG"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/portfolio/static/media/facebook.62d8b6ef.svg"
+  },
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/portfolio/static/media/github.ec5bc1b3.svg"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/portfolio/static/media/linkedin.d9049f46.svg"
+  },
+  {
+    "revision": "5683a0f18df026b373157c841e6dae55",
+    "url": "/portfolio/static/media/pc.5683a0f1.svg"
+  },
+  {
+    "revision": "7a98c111e15591b89d7bf182ab39b607",
+    "url": "/portfolio/static/media/women.7a98c111.svg"
+  }
+]);
