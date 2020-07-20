cac7214f1f53c7a31e29fd66b9dead4e5c6d810a
Dara <dara.dandashi@gmail.com>
2020-07-20 21:59:44 +0300

    deployed

--- src/Components/NavComponent.jsx
+++ src/Components/NavComponent.jsx
@@ -5,19 +5,19 @@ const NavComponent = () => {
   return (
     <div className="Nav">
       <MDBNavbar>
-        <MDBNavLink className="Navitem" to="/">
+        <MDBNavLink className="Navitem" to="/portfolio">
           Home
         </MDBNavLink>
-        <MDBNavLink className="Navitem" to="/About">
+        <MDBNavLink className="Navitem" to="/portfolio/About">
           About Me
         </MDBNavLink>
-        <MDBNavLink className="Navitem" to="/Bio">
+        <MDBNavLink className="Navitem" to="/portfolio/Bio">
           Bio
         </MDBNavLink>
-        <MDBNavLink className="Navitem" to="/Portfolio">
+        <MDBNavLink className="Navitem" to="/portfolio/Projects">
           Projects
         </MDBNavLink>
-        <MDBNavLink className="Navitem" to="/Contact">
+        <MDBNavLink className="Navitem" to="/portfolio/Contact">
           Contact
         </MDBNavLink>
       </MDBNavbar>
